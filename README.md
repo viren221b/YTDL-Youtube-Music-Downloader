2d126005db6a3d4aa7aebdcca9491b3f39635ab0
viren221b <314584579+viren221b@users.noreply.github.com>
2026-08-31 20:18:59 +0800

    add chapter splitting feature

---
+++ README.md
@@ -0,0 +1,24 @@
+# yt-music-dl
+
+A CLI tool to download YouTube videos as mp3.
+
+## Dependencies
+- Python 3
+- ffmpeg (install via your package manager)
+- deno (install via your package manager)
+
+## Installation
+1. git clone <your repo>
+2. pip install -r requirements.txt
+3. chmod +x yt-music-dl-main.py
+4. sudo ln -s /path/to/yt-music-dl-main.py /usr/local/bin/ytdl
+
+## Usage
+Just type `ytdl` in your terminal.
+
+##updates:
+ver 1.1: added a chapter split feature for yt videos that have split chapters
+
+##Supported Systems
+- This python script is mainly made for linux users, so you might wanna add linux sub-system for windows or mac if you're in that respective operating systems.
+
---
+++ ytdl-ver-1-1.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+#pseudo code:
+# 1. Paste URL
+# 2. Convert URL to mp3
+# 3. Option for file directory
+# 4. Save mp3 or m4a to file path
+# 
+# Additional Features (WIP):
+# 1. Playlist wide download (yt_dlp allows that
+# so i'll prolly just tweak the main code)
+# 2. Prompt user of playlist link (give
+# user the option to cancel or continue)
+
+
+#MAIN
+import yt_dlp
+
+def download_audio(video_url, save_path, no_playlist, split_chapters):
+    if split_chapters:
+        postprocessors = [
+            {'key': 'FFmpegSplitChapters'},
+            {'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '0'}
+        ]
+        outtmpl = {
+            'default': save_path + '/%(title)s/%(title)s.%(ext)s',
+            'chapter': save_path + '/%(title)s/%(section_title)s.%(ext)s'
+        }
+    else:
+        postprocessors = [
+            {'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '0'}
+        ]
+        outtmpl = save_path +'/%(title)s.%(ext)s'
+
+    ydl_opts = {
+         'noplaylist': no_playlist,
+         'format': 'bestaudio[ext=m4a]/bestaudio/best',
+         'outtmpl': outtmpl,
+         'postprocessors': postprocessors,
+         'verbose': True,
+         'writethumbnail': False,
+         'keepvideo': False,
+     }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+         ydl.download([video_url])
+
+while True:
+    yt_link = input("Paste YT link or playlist link here:\n") 
+    save_path = input("Input desired file path:\n")
+    
+    with yt_dlp.YoutubeDL({'quiet': True}) as ydl:
+        info = ydl.extract_info(yt_link, download=False)
+    
+    if "list=" in yt_link:
+        choice = input("This is a playlist link, download all? (y/n):\n")
+        if choice.lower() == 'y':
+            no_playlist = False
+        else:
+            no_playlist = True
+    else:
+        no_playlist = True
+
+    if info.get('chapters'):
+        choice = input("This video has chapters, want to split it? (y/n):\n")
+        if choice.lower() == 'y':
+            split_chapters = True
+        else:
+            split_chapters = False
+    else:
+        split_chapters = False
+
+    #create a loop function to continue the script otherwise give exit function
+    download_audio(yt_link, save_path, no_playlist, split_chapters)
+    again = input("Want to download another? (y/n):\n")
+    if again.lower() == 'n':
+        break
+
+        
+    
